dc227f6b357caf0587bfa06f4dacb25676d013a6
hftamayo <hftamayo@gmail.com>
2023-10-23 13:04:35 -0600

    adding routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
-import { useState } from 'react'
-import './App.css'
-import Layout from './components/shared/Layout'
+import React from "react";
+import "./App.css";
+import Layout from "./components/shared/Layout";
+import { Routes, Route } from "react-router-dom";
+import AllCakes from "./pages/AllCakes";
 
 function App() {
-
   return (
-    <Layout />
-  )
+    <Layout>
+      <Routes>
+        <Route path="/" element={<AllCakes />}></Route>
+      </Routes>
+    </Layout>
+  );
 }
 
-export default App
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import 'bootstrap/dist/css/bootstrap.min.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
     <App />
-)
+  </BrowserRouter>
+);
