21a7d5421d378518809d47c8a98f833beaf855c2
hftamayo <hftamayo@gmail.com>
2023-10-24 15:33:59 -0600

    first approach to display data

--- src/pages/AllCakes.js
+++ src/pages/AllCakes.js
@@ -1,7 +1,31 @@
+import { Row, Card, Container, Col } from "react-bootstrap";
+import { useCakeStore } from "../store/cakeStore";
+
 function AllCakes() {
+  const allCakes = useCakeStore((state) => state.cakeData);
   return (
-    <div>AllCakes</div>
-  )
+    <>
+      <Container className="mt-2">
+        <Row xs={1} md={3} className="g-4">
+          {allCakes.map((cake) => (
+            <Col key={cake.id}>
+              <Card>
+                <Card.Img
+                  variant="top"
+                  src={cake.imageUrl}
+                  style={{ height: 400, width: "100%" }}
+                />
+                <Card.Body>
+                  <Card.Title>{cake.name}</Card.Title>
+                  <Card.Text>Price - {cake.cost}</Card.Text>
+                </Card.Body>
+              </Card>
+            </Col>
+          ))}
+        </Row>
+      </Container>
+    </>
+  );
 }
 
-export default AllCakes
+export default AllCakes;
