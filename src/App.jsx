5dee51c323d596092e3600691410a7e033c41b90
hftamayo <hftamayo@gmail.com>
2023-10-25 15:12:25 -0600

    add-cake route

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import "./App.css";
 import Layout from "./components/shared/Layout";
 import { Routes, Route } from "react-router-dom";
 import AllCakes from "./pages/AllCakes";
+import AddCake from "./pages/AddCake";
 
 function App() {
   return (
     <Layout>
       <Routes>
-        <Route path="/" element={<AllCakes />}></Route>
+        <Route path="/view-cake" element={<AllCakes />}></Route>
+        <Route path="/add-cake" element={<AddCake />}></Route>
       </Routes>
     </Layout>
   );
