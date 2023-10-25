b687a15d86afbbef13fc77124ae5eb9135adf288
hftamayo <hftamayo@gmail.com>
2023-10-25 15:12:07 -0600

    addCake function

--- src/store/cakeStore.js
+++ src/store/cakeStore.js
@@ -1,4 +1,6 @@
-import {create} from "zustand";
+import axios from "axios";
+import { create } from "zustand";
+import { immer } from "zustand/middleware/immer";
 
 export const useCakeStore = create((set) => ({
   cakeData: [
@@ -6,25 +8,41 @@ export const useCakeStore = create((set) => ({
       id: 1,
       name: "Red Velvet",
       cost: 120,
-      imageUrl: "https://preppykitchen.com/wp-content/uploads/2022/07/Red-Velvet-Feature-1a.jpg",
+      imageUrl:
+        "https://preppykitchen.com/wp-content/uploads/2022/07/Red-Velvet-Feature-1a.jpg",
     },
     {
       id: 2,
       name: "Cherrie Pie",
       cost: 60,
-      imageUrl: "https://www.mybakingaddiction.com/wp-content/uploads/2023/08/cherry-pie-with-ice-cream-720x1080.jpg",
-    },    
+      imageUrl:
+        "https://www.mybakingaddiction.com/wp-content/uploads/2023/08/cherry-pie-with-ice-cream-720x1080.jpg",
+    },
     {
       id: 3,
       name: "Apple Pie",
       cost: 50,
-      imageUrl: "https://hips.hearstapps.com/hmg-prod/images/apple-pie-index-6425bd0363f16.jpg?crop=0.8888888888888888xw:1xh;center,top&resize=1200:*",
-    },    
+      imageUrl:
+        "https://hips.hearstapps.com/hmg-prod/images/apple-pie-index-6425bd0363f16.jpg?crop=0.8888888888888888xw:1xh;center,top&resize=1200:*",
+    },
     {
       id: 4,
       name: "Chocolate Cake",
       cost: 100,
-      imageUrl: "https://inbloombakery.com/wp-content/uploads/2023/02/chocolate-ganache-cake_-12.jpg",
-    },    
+      imageUrl:
+        "https://inbloombakery.com/wp-content/uploads/2023/02/chocolate-ganache-cake_-12.jpg",
+    },
   ],
+  getCakesAPI: async () => {
+    const apiResponse = await axios.get("");
+    set((state) => {
+      state.cakeData = apiResponse.data;
+    });
+  },
+  addCakeAPI: async (payload) => {
+    const apiResponse = await axios.post("", payload);
+    set((state) => {
+      state.cakeData.push(apiResponse.data);
+    });
+  },
 }));
