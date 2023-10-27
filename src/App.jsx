f4b54f35404a37958fb689410e5396b287299d9c
hftamayo <hftamayo@gmail.com>
2023-10-27 15:47:07 -0600

    editCake endpoint

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Layout from "./components/shared/Layout";
 import { Routes, Route } from "react-router-dom";
 import AllCakes from "./pages/AllCakes";
 import AddCake from "./pages/AddCake";
+import EditCake from "./pages/EditCake";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path="/view-cake" element={<AllCakes />}></Route>
         <Route path="/add-cake" element={<AddCake />}></Route>
+        <Route path="/edit-cake/:id" element={<EditCake />}></Route>
       </Routes>
     </Layout>
   );
