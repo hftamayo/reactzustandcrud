9ccbb06a537baf8b8225e50da36454c6a1a36e8b
hftamayo <hftamayo@gmail.com>
2023-10-27 14:40:02 -0600

    adding navigation and routes

--- src/pages/AllCakes.jsx
+++ src/pages/AllCakes.jsx
@@ -1,18 +1,35 @@
 import { Row, Card, Container, Col } from "react-bootstrap";
 import { useCakeStore } from "../store/cakeStore";
 import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
-function AllCakes() {
+const AllCakes = () => {
   const allCakes = useCakeStore((state) => state.cakeData);
   const getCakesAPICall = useCakeStore((state) => state.getCakesAPICall);
+  const navigate = useNavigate();
 
   useEffect(() => {
-    getCakesAPICall();
+    if (allCakes.length == 0) {
+      getCakesAPICall();
+    }
   }, []);
-  
+
   return (
     <>
       <Container className="mt-2">
+        <Row>
+          <Col className="col-md-4 offset-md-4">
+            <Button
+              variant="primary"
+              type="button"
+              onClick={() => {
+                navigate("/add-cake");
+              }}
+            >
+              Add
+            </Button>
+          </Col>
+        </Row>
         <Row xs={1} md={3} className="g-4">
           {allCakes.map((cake) => (
             <Col key={cake.id}>
@@ -33,6 +50,6 @@ function AllCakes() {
       </Container>
     </>
   );
-}
+};
 
 export default AllCakes;
