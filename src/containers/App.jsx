e1516ef8cc1dbc9160561d184077e8a7c966f46d
hftamayo <hftamayo@gmail.com>
2023-11-06 14:23:02 -0600

    setting root component

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -4,11 +4,13 @@ import { Routes, Route } from "react-router-dom";
 import AllCakes from "../components/entities/Cakes/AllCakes";
 import AddCake from "../components/entities/Cakes/AddCake";
 import EditCake from "../components/entities/Cakes/EditCake";
+import Home from "../components/Home";
 
 function App() {
   return (
     <Layout>
       <Routes>
+        <Route path="/" element={<Home />}></Route>
         <Route path="/view-cake" element={<AllCakes />}></Route>
         <Route path="/add-cake" element={<AddCake />}></Route>
         <Route path="/edit-cake/:id" element={<EditCake />}></Route>
