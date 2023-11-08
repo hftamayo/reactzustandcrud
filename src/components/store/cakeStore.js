532c1c1c83454fe1328ecdec7debf0663ab66473
hftamayo <hftamayo@gmail.com>
2023-11-07 20:22:34 -0600

    adding localstorage support

--- src/components/store/cakeStore.js
+++ src/components/store/cakeStore.js
@@ -6,45 +6,19 @@ import { immer } from "zustand/middleware/immer";
 export const useCakeStore = create(
   devtools(
     immer((set) => ({
-      cakeData: [
-        {
-          id: 1,
-          name: "Red Velvet",
-          cost: 120,
-          imageUrl:
-            "https://preppykitchen.com/wp-content/uploads/2022/07/Red-Velvet-Feature-1a.jpg",
-        },
-        {
-          id: 2,
-          name: "Cherrie Pie",
-          cost: 60,
-          imageUrl:
-            "https://www.mybakingaddiction.com/wp-content/uploads/2023/08/cherry-pie-with-ice-cream-720x1080.jpg",
-        },
-        {
-          id: 3,
-          name: "Apple Pie",
-          cost: 50,
-          imageUrl:
-            "https://hips.hearstapps.com/hmg-prod/images/apple-pie-index-6425bd0363f16.jpg?crop=0.8888888888888888xw:1xh;center,top&resize=1200:*",
-        },
-        {
-          id: 4,
-          name: "Chocolate Cake",
-          cost: 100,
-          imageUrl:
-            "https://inbloombakery.com/wp-content/uploads/2023/02/chocolate-ganache-cake_-12.jpg",
-        },
-      ],
+      cakeData: [],
 
       getCakesAPI: async () => {
         set((state) => {
-          return state.cakeData;
+          return localStorage.getItem("cakes")
+            ? JSON.parse(localStorage.getItem("cakes"))
+            : [];
         });
       },
       addCakeAPI: async (payload) => {
         set((state) => {
           state.cakeData.push(payload);
+          localStorage.setItem("cakes", JSON.stringify(state.cakeData));
         });
       },
       updateCakeAPI: async (payload) => {
@@ -61,8 +35,8 @@ export const useCakeStore = create(
       },
 
       // methods using API calls
-      // getCakesAPI: async () => {
       //   const apiResponse = await axios.get("");
+      // getCakesAPI: async () => {
       //   set((state) => {
       //     state.cakeData = apiResponse.data;
       //   });
