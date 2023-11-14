62d359b88cf3cae2d28998b87ebb97b19dd80686
hftamayo <hftamayo@gmail.com>
2023-11-14 15:36:23 -0600

    main component test

---
+++ src/test/main.test.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
+import App from '../containers/App';
+
+test('renders App component within BrowserRouter', () => {
+  const { getByTestId } = render(
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  );
+
+  // Check if the App component renders within BrowserRouter
+  const appComponent = getByTestId('app-component');
+  expect(appComponent).toBeInTheDocument();
+});
